ced789ee29746ddbb351f9163fe313d9355d3d20
vinayakparmar92 <vinayakparmar1992@gmail.com>
2018-09-20 16:22:10 +0530

    Initial Commit

---
+++ VPProgressingPageControl/VPProgressingPageControl.h
@@ -0,0 +1,19 @@
+//
+//  VPProgressingPageControl.h
+//  VPProgressingPageControl
+//
+//  Created by Vinayak Parmar on 20/09/18.
+//  Copyright © 2018 VMP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPProgressingPageControl.
+FOUNDATION_EXPORT double VPProgressingPageControlVersionNumber;
+
+//! Project version string for VPProgressingPageControl.
+FOUNDATION_EXPORT const unsigned char VPProgressingPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPProgressingPageControl/PublicHeader.h>
+
+
